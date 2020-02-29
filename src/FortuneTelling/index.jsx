91770bd8a87be6bed53f9cd1d4a4bcf4b90f73b2
Mandeep <mkaur@pagerduty.com>
2020-02-29 11:37:21 -0500

    Setup fortune telling ball

---
+++ src/FortuneTelling/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import MagicBall from '../Images/MagicBall.svg'
+
+const FortuneTelling = () => (
+    <img src={MagicBall} style={{'height': '360px'}} alt='fortune telling ball'/>
+)
+export default FortuneTelling
--- src/Homepage/index.jsx
+++ src/Homepage/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './homepage.css'
+import FortuneTelling from '../FortuneTelling'
 
 const HomePage = () => (
-  <div className='homepage'>
-    Bots Agaisnt Bangs
+  <div className='homepage fixed-top'>
+    <h1> Bots Agaisnt Bangs </h1>
+    <FortuneTelling/>
   </div>
 )
 export default HomePage
