efe3929a8a632c0d4fe7554c1c8bb8b55a864b4f
Mandeep <mkaur@pagerduty.com>
2020-02-29 14:23:48 -0500

    Conditionally Display fortune teller path

--- src/FortuneTelling/index.jsx
+++ src/FortuneTelling/index.jsx
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
-import MagicBall from '../Images/MagicBall.svg'
-import FortuneTellingBall from '../Images/FortuneTellingBall.svg'
 import sample from 'lodash.sample';
+import MagicBall from '../Images/MagicBall.svg';
+import FortuneTellingBall from '../Images/FortuneTellingBall.svg';
+import './style.css';
 
 const FORTUNE_RESPONSE = [
-"It is not adviseable.",
-"It's a no for me, dog.",
-"Girl, no. ",
-"Don't you dare.",
-"Are you okay?",
-"This idea will pass.",
-"Not this again.",
-"Go to therapy. ",
-"Ask again later.",
-"Remember last time?",
-"Outlook not so good",
-"It is certain...ly a bad idea",
-"Let's sleep on it.",
-"Call your BFF.",
-"I'm not convinced.",
-"Noooooooooope.",
-"No, no, no.",
-"Learn from my mistakes",
-]
+  'It is not adviseable.',
+  "It's a no for me, dog.",
+  'Girl, no. ',
+  "Don't you dare.",
+  'Are you okay?',
+  'This idea will pass.',
+  'Not this again.',
+  'Go to therapy. ',
+  'Ask again later.',
+  'Remember last time?',
+  'Outlook not so good',
+  'It is certain...ly a bad idea',
+  "Let's sleep on it.",
+  'Call your BFF.',
+  "I'm not convinced.",
+  'Noooooooooope.',
+  'No, no, no.',
+  'Learn from my mistakes',
+];
 
-class FortuneTelling extends React.Component {
+class FortuneTelling extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentFortune: null
-    }
+      currentFortune: null,
+    };
   }
 
   setFortune = () => {
@@ -37,16 +38,22 @@ class FortuneTelling extends React.Component {
   };
 
   render() {
-  return(
-    <div>
-      <img src={FortuneTellingBall}
-      onClick={this.setFortune}
-      style={{'height': '360px'}}
-      alt='fortune telling ball'
-      />
-      {this.state.currentFortune}
+    if (this.props.isVisible) {
+      return(
+      <div>
+        <img
+          src={FortuneTellingBall}
+          onClick={this.setFortune}
+          style={{ height: '360px' }}
+          alt="fortune telling ball"
+        />
+        <div>
+          <h2>{this.state.currentFortune}</h2>
+        </div>
       </div>
-  )
+      )
+    }
+    return null;
   }
 }
-export default FortuneTelling
+export default FortuneTelling;
--- src/Homepage/index.jsx
+++ src/Homepage/index.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import './homepage.css'
-import FortuneTelling from '../FortuneTelling'
+import React, { Component } from 'react';
+import './homepage.css';
+import { Button } from 'reactstrap';
+import FortuneTelling from '../FortuneTelling';
 
-const HomePage = () => (
-  <div className='homepage fixed-top'>
-    <h1> Bots Agaisnt Bangs </h1>
-    <FortuneTelling/>
-  </div>
-)
-export default HomePage
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showFortuneTeller: false,
+      showSelectionPath: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className="homepage fixed-top">
+        <h1> Should I get Bangs? </h1>
+        <Button onClick={() => this.setState({ showFortuneTeller: true })}>Ask A fortune Teller</Button>
+        <FortuneTelling isVisible={this.state.showFortuneTeller} />
+      </div>
+    );
+  }
+}
+export default HomePage;
